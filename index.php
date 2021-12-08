6272c7d41e8f92b81cf96c9df23e9ed4bfc40e20
Sanjay Nithish K S <66366859+Sanjay-Nithish-KS@users.noreply.github.com>
2021-12-08 15:41:07 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
-$db_user='root';
-$db_pass='root';
+$db_user='admin';
+$db_pass='adminadmin';
 $link = mysqli_connect('localhost:3306',"$db_user", "$db_pass");
 if (!$link) {
         die('Could not connect to MYSQL ');
